0ce9037ec40266445daca9214071a8d92e1f53b0
Dipendra <deardipen50@gmail.com>
2026-03-22 15:46:42 +0100

    chore: update export type

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  output: "export",
 };
 
 export default nextConfig;
